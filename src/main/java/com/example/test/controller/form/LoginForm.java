a8e1db3e250bcf4d673bb4eba7d27ba87df548d8
root24 <root24@10.248.148.9>
2017-11-11 13:55:52 +0900

    pom.xmlにvalidation追加

--- src/main/java/com/example/test/controller/form/LoginForm.java
+++ src/main/java/com/example/test/controller/form/LoginForm.java
@@ -2,6 +2,29 @@ package com.example.test.controller.form;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+
 public class LoginForm implements Serializable {
 
+	@NotNull
+	private String e_mail;
+
+	@NotNull
+	private String password;
+
+	public String getE_mail() {
+		return e_mail;
+	}
+
+	public void setE_mail(String e_mail) {
+		this.e_mail = e_mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
